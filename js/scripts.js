4ff2f8bd5739272d9b23ada7e56a2bb5f1d6b3ac
Machoka Daisy <machokadaisy25@gmail.com>
2020-08-03 02:43:08 +0300

    add jQuery rule to alert the user that their order will be delivered to inputted location

--- js/scripts.js
+++ js/scripts.js
@@ -2,4 +2,8 @@ $(document).ready(function(){
     $("#yesDelivery").click(function(){
         $(".location").show();
     })
+    $("#bttn1").click(function(){
+        var userLocation= document.getElementById("deliveryLocation");
+        alert("Your order will be delivered to " + userLocation.value);
+    })
 })
