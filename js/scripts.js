c9ada75bd17b28bd364f34a1a90deb3b4fa0ba2f
Machoka Daisy <machokadaisy25@gmail.com>
2020-08-03 15:50:23 +0300

    add a new constructor for pizza size

--- js/scripts.js
+++ js/scripts.js
@@ -6,4 +6,11 @@ $(document).ready(function(){
         var userLocation= document.getElementById("deliveryLocation");
         alert("Your order will be delivered to " + userLocation.value);
     })
+    function Pizzasize(size, price) {
+        this.size= size;
+        this.price= price;
+    }
+    var large= new Pizzasize("large", 350);
+    var medium= new Pizzasize("medium", 250);
+    var small= new Pizzasize("small", 150)
 })
