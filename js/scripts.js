7aad853644355e9f61435a9b08b8f818fa84762b
Machoka Daisy <machokadaisy25@gmail.com>
2020-08-03 23:37:21 +0300

    add a javascript constructor for pizza orders

--- js/scripts.js
+++ js/scripts.js
@@ -6,11 +6,30 @@ $(document).ready(function(){
         var userLocation= document.getElementById("deliveryLocation");
         alert("Your order will be delivered to " + userLocation.value);
     })
-    function Pizzasize(size, price) {
-        this.size= size;
-        this.price= price;
-    }
-    var large= new Pizzasize("large", 350);
-    var medium= new Pizzasize("medium", 250);
-    var small= new Pizzasize("small", 150)
+    $("#orderNow").click(function(){
+        $("#orderAnother").show();
+    })
+    event.preventDefault();
+    
+
 })
+var orders=[];
+function Order(size, crust, toppings, delivery) {
+    this.size=size;
+    this.crust=crust;
+    this.toppings=toppings;
+    this.delivery=delivery;
+}
+var order= new Order(
+    document.getElementsByName("size").value,
+    document.getElementsByName("crust").value,
+    document.getElementsByName("toppings").value,
+    document.getElementsByName("delivery").value,
+)
+orders.push(order);
+
+var size=document.getElementsByName("size").value;
+var crust= document.getElementsByName("crust").value;
+var toppings=document.getElementsByName("toppings").value;
+var delivery=document.getElementsByName("delivery").value;
+
