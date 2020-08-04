d2b4e08a93fa84c170cc0c917a07215b5e64330d
Machoka Daisy <machokadaisy25@gmail.com>
2020-08-04 07:40:58 +0300

    add a javascript function to calculate total cost of an order

--- js/scripts.js
+++ js/scripts.js
@@ -28,8 +28,84 @@ var order= new Order(
 )
 orders.push(order);
 
-var size=document.getElementsByName("size").value;
-var crust= document.getElementsByName("crust").value;
-var toppings=document.getElementsByName("toppings").value;
-var delivery=document.getElementsByName("delivery").value;
+var totalCost= function(){
+    var large=document.getElementById("pizzaLarge");
+    var medium= document.getElementById("pizzaMedium");
+    var small=document.getElementById("pizzaSmall");
+    var stuffed=document.getElementById("crustStuffed");
+    var crispy=document.getElementById("crustCrispy");
+    var glutenFree=document.getElementById("crustGlutenFree");
+    var chicken=document.getElementById("bbqChickenToppings");
+    var pepperoni=document.getElementById("pepperoniToppings");
+    var veggiemix=document.getElementById("veggieToppings");
+    var yesDelivery=document.getElementById("yesDelivery");
+    var noDelivery=document.getElementById("noDelivery");
 
+
+    function Ordercost(sizeCost, crustCost, toppingsCost, deliveryFee) {
+        this.sizeCost=sizeCost;
+        this.crustCost=crustCost;
+        this.toppingsCost=toppingsCost;
+        this.deliveryFee=deliveryFee;
+    }
+    var sizeCost= function(){
+        if(large.checked===true){
+            return sizeCost=350;
+        } else if(medium.checked===true) {
+            return sizeCost=250;
+        } else if(small.checked===true) {
+            return sizeCost=150;
+        } 
+    }
+    var crustCost= function(){
+        if(stuffed.checked===true){
+            return crustCost=300;
+        } else if(crispy.checked===true) {
+            return crustCost=200;
+        } else if(glutenFree.checked===true) {
+            return crustCost=400;
+        } 
+    }
+    var toppingsCost= function(){
+        if(chicken.checked===true){
+            return toppingsCost=350;
+        } else if(pepperoni.checked===true) {
+            return toppingsCost=250;
+        } else if(veggiemix.checked===true) {
+            return toppingsCost=150;
+        } 
+    }
+    var deliveryFee= function(){
+        if(yesDelivery.checked===true&& noDelivery.checked===false){
+            return deliveryFee=150;
+        } else if(noDelivery.checked===true&& yesDelivery.checked===false) {
+            return deliveryFee=0;
+        } else {
+            return false;
+        }
+    }
+    var newSizeCost= sizeCost();
+    var newCrustCost= crustCost();
+    var newToppingsCost= toppingsCost();
+    var newDeliveryFee= deliveryFee();
+    var total=0
+    var newTotalCost= function() {
+        return total+=newSizeCost+ newCrustCost + newToppingsCost + newDeliveryFee;
+    }
+    var overallCost= newTotalCost();
+    alert("your bill is "+ overallCost);
+    document.getElementById("userCost").innerHTML= overallCost;
+
+    
+}
+var deliveryLocator= function(){
+    var userLocation= document.getElementById("deliveryLocation");
+    var yesDelivery=document.getElementById("yesDelivery");
+    var noDelivery=document.getElementById("noDelivery");
+
+    if(yesDelivery.checked===true&& noDelivery.checked===false){
+        alert("Your order will be delivered to "+userLocation.value+ ". Delivery fee is Kshs.150");
+    } else if(noDelivery.checked===true&& yesDelivery.checked===false) {
+        "";
+    } 
+}
